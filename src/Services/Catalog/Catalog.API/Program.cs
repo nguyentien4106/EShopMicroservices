a136eb4b5301ab649378c6752ea57e06266cc9e6
walter.nguyen <walter.nguyen@nexcel.com>
2025-02-11 17:28:56 +0700

    add create product

---
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -0,0 +1,15 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+
+var app = builder.Build();
+
+app.MapCarter();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
