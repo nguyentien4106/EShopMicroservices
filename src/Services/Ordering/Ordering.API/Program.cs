1ba042406eab68ca6b4c0572b2b307d387414c7d
walter.nguyen <walter.nguyen@nexcel.com>
2025-03-05 17:59:28 +0700

    add ordering service using clean architect

---
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -0,0 +1,16 @@
+using Ordering.API;
+using Ordering.Application;
+using Ordering.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddApplicationServices()
+    .AddInfrastructureServices(builder.Configuration)
+    .AddApiServices(); ;
+
+var app = builder.Build();
+app.UseApiServices();
+
+app.MapGet("/", () => "Ordering API");
+
+app.Run();
---
+++ src/Services/Ordering/Ordering.Domain/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using Ordering.Domain.Abstractions;
+
+namespace Ordering.Domain.Models;
+
+public class Product : Entity<ProductId>
+{
+    public string Name { get; private set; } = default!;
+
+    public decimal Price { get; private set; } = default!;
+
+    public static Product Create(ProductId id, string name, decimal price) 
+        => new()
+            {
+                Id = id,
+                Name = name,
+                Price = price,
+            };
+}
